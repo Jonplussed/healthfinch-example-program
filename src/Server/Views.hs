7b29a742f607adf28bb4b95befb0cb6bc88e1605
Jon Childress <jon@childr.es>
2015-07-04 12:29:48 -0500

    add undefined views

---
+++ src/Server/Views.hs
@@ -0,0 +1,24 @@
+module Server.Views where
+( homeView
+, indexView
+, showView
+, error404View
+, error500View
+) where
+
+import Text.Blaze.Html (Html)
+
+homeView :: Html
+homeView = undefined
+
+indexView :: Html
+indexView = undefined
+
+showView :: Html
+showView = undefined
+
+error404View :: Html
+error404View = undefined
+
+error500View :: Html
+error500View = undefined
