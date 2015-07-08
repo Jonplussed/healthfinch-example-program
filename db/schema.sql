f1d9dabcd674d72f4c5aec903ec5801668784274
Jon Childress <jon@childr.es>
2015-07-08 17:04:58 -0500

    formatting

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,9 @@
 CREATE EXTENSION citext;
 
 CREATE TABLE histogram (
-  url          CITEXT     NOT NULL,
-  word         CITEXT     NOT NULL,
-  count        INTEGER    NOT NULL
+  url        CITEXT   NOT NULL,
+  word       CITEXT   NOT NULL,
+  frequency  INTEGER  NOT NULL
 );
 
 CREATE UNIQUE INDEX url_and_word_on_histogram
